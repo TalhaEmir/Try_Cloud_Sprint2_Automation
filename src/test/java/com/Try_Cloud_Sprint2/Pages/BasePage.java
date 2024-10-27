954c34d0a979436af958cc787c5cad2fae4cdcf4
TalhaDoganGit <talha.emir.dogan@gmail.com>
2024-10-27 16:42:59 -0400

    test feature chanegs

--- src/test/java/com/Try_Cloud_Sprint2/Pages/BasePage.java
+++ src/test/java/com/Try_Cloud_Sprint2/Pages/BasePage.java
@@ -16,32 +16,15 @@ import java.util.List;
 
 public abstract class BasePage {
 
-    @FindBy(css = "span.title-level-1")
-    public List<WebElement> menuOptions;
-
-    @FindBy(css = "div[class='loader-mask shown']")
-    @CacheLookup
-    protected WebElement loaderMask;
-
-    @FindBy(css = "h1[class='oro-subtitle']")
-    public WebElement pageSubTitle;
-
-    @FindBy(css = "#user-menu > a")
-    public WebElement userName;
-
-    @FindBy(linkText = "Logout")
-    public WebElement logOutLink;
-
-    @FindBy(linkText = "My User")
-    public WebElement myUser;
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-public void clickMenuByText(String text){
-String locator="(//a[contains(.,'"+text+"')])[1]";
-Driver.getDriver().findElement(By.xpath(locator));
+    public void clickMenuByText(String text) {
+        String locator = "(//a[contains(.,'" + text + "')])[1]";
+        Driver.getDriver().findElement(By.xpath(locator)).click();
+    }
 
-}
+
 }
