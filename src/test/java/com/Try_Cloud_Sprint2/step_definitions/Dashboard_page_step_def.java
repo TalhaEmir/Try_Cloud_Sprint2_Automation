528d570b9e248f2e8e8f4cf15334fe3aa13f0af9
cristakubik <cristakubik@gmail.com>
2024-10-28 11:42:51 -0400

    added Dashboard_page_step_def

---
+++ src/test/java/com/Try_Cloud_Sprint2/step_definitions/Dashboard_page_step_def.java
@@ -0,0 +1,23 @@
+package com.Try_Cloud_Sprint2.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Dashboard_page_step_def {
+
+
+    @Given("After Login user can see all modules and Username.")
+    public void after_login_user_can_see_all_modules_and_username() {
+
+    }
+    @Then("User can click on Customize button and select any of the Widgets.")
+    public void user_can_click_on_customize_button_and_select_any_of_the_widgets() {
+
+    }
+    @Then("User can click on Set Status button and select any of the Status options.")
+    public void user_can_click_on_set_status_button_and_select_any_of_the_status_options() {
+
+    }
+
+
+}
