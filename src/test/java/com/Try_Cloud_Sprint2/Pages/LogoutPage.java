d6ba6127cf5b000cb00110539fe426ead9eac8dc
NelyaW <nelyawiebe@gmail.com>
2024-10-31 13:05:35 -0400

    Updated Logout feature, page, and step definitions

--- src/test/java/com/Try_Cloud_Sprint2/Pages/LogoutPage.java
+++ src/test/java/com/Try_Cloud_Sprint2/Pages/LogoutPage.java
@@ -2,6 +2,7 @@
 package com.Try_Cloud_Sprint2.Pages;
 
 import com.Try_Cloud_Sprint2.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -11,15 +12,21 @@ public class LogoutPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(id = "logout")
+    @FindBy(xpath = "/html[1]/body[1]/header[1]/div[2]/div[4]/div[1]/div[1]/img[1]")
+    public WebElement logoutHeader;
+
+    public void logoutHeader(){
+        this.logoutHeader.click();
+    }
+    @FindBy(xpath = "//a[normalize-space()='Log out']")
     public WebElement logoutButton;
 
-    //@FindBy()             //I dont know how to locate the web element for step back button
-    // or if we just verify it by comparing page title names
-    //public WebElement step_back_button;
+
 
     public void logout(){
         this.logoutButton.click();
     }
+
+
 
 }
--- src/test/java/com/Try_Cloud_Sprint2/step_definitions/Logout_Step_Definitions.java
+++ src/test/java/com/Try_Cloud_Sprint2/step_definitions/Logout_Step_Definitions.java
@@ -1,5 +1,4 @@
 
-
 package com.Try_Cloud_Sprint2.step_definitions;
 
 import com.Try_Cloud_Sprint2.Pages.BasePage;
@@ -15,10 +14,16 @@ public class Logout_Step_Definitions extends BasePage {
     LogoutPage logoutPage = new LogoutPage();
     LoginPage loginPage = new LoginPage();
 
-    @When("User clicks logout button")
+
+    @When("User clicks logout header button")
+    public void userClicksLogoutHeaderButton() {
+        logoutPage.logoutHeader.click();
+    }
+    @Then("User clicks logout button")
     public void user_clicks_logout_button() {
         logoutPage.logoutButton.click();
     }
+
     @Then("User should be logged out and redirected to login page")
     public void user_should_be_logged_out_and_redirected_to_login_page() {
         String actualTitle = Driver.getDriver().getTitle();
@@ -26,7 +31,6 @@ public class Logout_Step_Definitions extends BasePage {
 
         Assert.assertEquals(actualTitle, expectedTitle);
 
-        Driver.closeDriver();
     }
 
     @And("User cannot return to homepage by clicking step back button after logout")
