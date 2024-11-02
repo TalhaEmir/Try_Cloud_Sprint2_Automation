0c3a5e9d94b6c24145ee839271c17c46d5ec4607
taieb <ouberka021@gmail.com>
2024-11-01 22:47:53 -0400

    add new group page

--- src/test/java/com/Try_Cloud_Sprint2/Pages/ContactGroupPage.java
+++ src/test/java/com/Try_Cloud_Sprint2/Pages/ContactGroupPage.java
@@ -9,40 +9,40 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 public class ContactGroupPage {
-    public ContactGroupPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public ContactGroupPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//span[contains(text(),'New group')]")
     public WebElement addGroup;
 
-    @FindBy(xpath = "//form[@class='action-input__form']//input[@type='text']")
+    @FindBy(xpath = "//input[@class='action-input__input focusable']")
     public WebElement enterGroupName;
 
-    @FindBy(xpath = "//span[contains(.,'All cont')]")
+    @FindBy(xpath = "//span[@class='app-navigation-entry__title']")
     public WebElement allContactsLink;
 
-@FindBy(xpath = "//input[@placeholder='Add contact in group']")
-   public WebElement inputGroup;
-@FindBy(xpath = "//div[@class='name-parts']/span[.='Group 100']")
+    @FindBy(xpath = "//input[@placeholder='Add contact in group']")
+    public WebElement inputGroup;
+    @FindBy(xpath = "//div[@class='name-parts']/span[.='group10']")
     public WebElement groupName;
 
 
-@FindBy(xpath = "//input[@placeholder='Choose property type']")
-public WebElement inputProperty;
+    @FindBy(xpath = "//input[@placeholder='Choose property type']")
+    public WebElement inputProperty;
 
-public void selectProperty(String propertyType){
-    List<WebElement> options = Driver.getDriver().findElements(By.xpath("//li[@class='multiselect__element']"));
-    for(WebElement option : options){
-        System.out.println(option.getText());
+    public void selectProperty(String propertyType) {
+        List<WebElement> options = Driver.getDriver().findElements(By.xpath("//li[@class='multiselect__element']"));
+        for (WebElement option : options) {
+            System.out.println(option.getText());
 
-        if(option.getText().contains(propertyType)){
-            option.click();
-            break;
+            if (option.getText().contains(propertyType)) {
+                option.click();
+                break;
+            }
         }
     }
-}
 
-    }
+}
 
 
